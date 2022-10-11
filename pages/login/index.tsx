5920001279064b8b97dcf983e1094de02bca8cac
vrn295 <varun.org295@gmail.com>
2022-10-11 11:45:56 +0530

    FEAT: mvvm model in login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,71 +1,21 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import SpotifyLoader from "../../src/common/SpotifyLoader";
-import { ELocalStorage } from "../../src/constants";
-import { ERoutes } from "../../src/constants/Routes";
-import { getUserData } from "../../src/services/services";
+import useLoginView from "../../src/hooks/useLoginView";
 
 const Login = () => {
-  const router = useRouter()
-  const openLogin = () => {
-
-    function generateRandomString(length) {
-      var result = '';
-      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var charactersLength = characters.length;
-      for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() *
-          charactersLength));
-      }
-      return result;
-    }
-
-    var state = generateRandomString(16);
-
-    // localStorage.setItem(stateKey, state);
-    var scope = 'user-read-private user-read-email user-top-read playlist-read-private streaming';
-
-    var url = 'https://accounts.spotify.com/authorize';
-    url += '?response_type=token';
-    url += '&client_id=' + encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID);
-    url += '&scope=' + encodeURIComponent(scope);
-    url += '&show_dialog=true';
-    url += '&redirect_uri=' + encodeURIComponent(`${process.env.NEXT_PUBLIC_WEB_URL}/login`);
-    url += '&state=' + encodeURIComponent(state);
-    window.location.href = url
-  }
+  const router = useRouter();
+  const { loginStatusCheck } = useLoginView();
 
   useEffect(() => {
-    if (localStorage.getItem(ELocalStorage.Token)) {
-      checkTokenExpire()
-    } else {
-      const access_token = window.location.hash?.match(/\#(?:access_token)\=([\S\s]*?)\&/)?.length ? window.location.hash?.match(/\#(?:access_token)\=([\S\s]*?)\&/)![1] : ''
-      if (access_token && localStorage.getItem(ELocalStorage.Token) !== access_token) {
-        localStorage.setItem(ELocalStorage.Token, access_token)
-        router.push(ERoutes.PROFILE)
-      } else {
-        openLogin()
-      }
-    }
-  }, [router])
-
-  const checkTokenExpire = async () => {
-    try {
-      const result = await getUserData(localStorage.getItem(ELocalStorage.Token))
-      if(result) {
-        router.push(ERoutes.PROFILE)
-      }
-    } catch {
-      localStorage.removeItem(ELocalStorage.Token)
-      openLogin()
-    }
-  } 
+    loginStatusCheck();
+  }, [router]);
 
   return (
     <div>
       <SpotifyLoader />
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/hooks/useLoginView.tsx
@@ -0,0 +1,70 @@
+import { useRouter } from "next/router";
+import { ELocalStorage } from "../constants";
+import { ERoutes } from "../constants/Routes";
+import { getUserData } from "../services/services";
+import { generateRandomString } from "../utils";
+
+const useLoginView = () => {
+  const router = useRouter();
+
+  const openLogin = () => {
+    var state = generateRandomString(16);
+
+    var scope =
+      "user-read-private user-read-email user-top-read playlist-read-private streaming";
+
+    var url = "https://accounts.spotify.com/authorize";
+    url += "?response_type=token";
+    url +=
+      "&client_id=" +
+      encodeURIComponent(process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID);
+    url += "&scope=" + encodeURIComponent(scope);
+    url += "&show_dialog=true";
+    url +=
+      "&redirect_uri=" +
+      encodeURIComponent(`${process.env.NEXT_PUBLIC_WEB_URL}/login`);
+    url += "&state=" + encodeURIComponent(state);
+    window.location.href = url;
+  };
+
+  const loginStatusCheck = () => {
+    if (localStorage.getItem(ELocalStorage.Token)) {
+      checkTokenExpire();
+    } else {
+      const access_token = window.location.hash?.match(
+        /\#(?:access_token)\=([\S\s]*?)\&/
+      )?.length
+        ? window.location.hash?.match(/\#(?:access_token)\=([\S\s]*?)\&/)![1]
+        : "";
+      if (
+        access_token &&
+        localStorage.getItem(ELocalStorage.Token) !== access_token
+      ) {
+        localStorage.setItem(ELocalStorage.Token, access_token);
+        router.push(ERoutes.PROFILE);
+      } else {
+        openLogin();
+      }
+    }
+  };
+
+  const checkTokenExpire = async () => {
+    try {
+      const result = await getUserData(
+        localStorage.getItem(ELocalStorage.Token)
+      );
+      if (result) {
+        router.push(ERoutes.PROFILE);
+      }
+    } catch {
+      localStorage.removeItem(ELocalStorage.Token);
+      openLogin();
+    }
+  };
+
+  return {
+    loginStatusCheck,
+  };
+};
+
+export default useLoginView;
